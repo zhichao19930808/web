f9ba52ad9d26ffb8032d2303bd1b0e0a86e552f9
zhichao19930808 <zhichaoglory@163.com>
2017-08-01 17:55:54 +0800

    实现javax.servlet.Servlet接口

---
+++ src/main/java/ServletTest01.java
@@ -0,0 +1,36 @@
+import javax.servlet.*;
+import java.io.IOException;
+
+public class ServletTest01 implements Servlet{
+    //servlet第一次访问时调用该方法，服务器会调用此方法会创建一个此类的实例
+    public ServletTest01() {
+        System.out.println("-------构造方法-------");
+    }
+    //servlet 第一次访问时会调用该方法，只会调用一次
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("----------init:------------------");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+        //servlet 每次访问此类都会调用此方法
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("--------service:----------------------");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    //销毁时调用此方法
+    @Override
+    public void destroy() {
+        System.out.println("-------------destory:-----------------------");
+    }
+}
