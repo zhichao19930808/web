7995812e4669544bb1e9ad40037f41da9e573b80
zhichao19930808 <zhichaoglory@163.com>
2017-08-02 16:24:05 +0800

    修改服务器和浏览器编码格式并由服务器向浏览器发出一条字符流

---
+++ src/main/java/com/cd/ServletCodingTest.java
@@ -0,0 +1,32 @@
+package com.cd;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletCodingTest extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//修改编码格式
+//  方法1：
+        //告诉服务器使用什么编码格式解析文本
+        //resp.setCharacterEncoding("UTF-8");
+        //告诉浏览器用什么编码格式解析文本
+       // resp.setHeader("content","charset=utf-8");
+//  方法2：
+        resp.setContentType("charset = utf-8");
+        //获取字符输出流
+//测试：
+        PrintWriter writer = resp.getWriter();
+        //向浏览器输出字符流
+        writer.write("hello");
+    }
+}
